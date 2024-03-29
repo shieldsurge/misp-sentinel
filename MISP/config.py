00b4a5be2286b6f8dba8f8f66747210a462aaeed
shieldsurge <110939943+shieldsurge@users.noreply.github.com>
2024-03-28 23:02:14 -0400

    Add security-api-solutions code directly to repo
    Add code from microsoftgraph security-api-solutions repo to this repo due to security-api-solutions repo being deleted from GitHub.

---
+++ MISP/config.py
@@ -0,0 +1,17 @@
+graph_auth = {
+    'tenant': '844ef997-7b63-48f0-882a-7dc8162e363b',
+    'client_id': 'bcc21a63-86a6-4423-a4dd-b095b7148965',
+    'client_secret': '43O8Q~tNM1gniVhXAyhAbOn-5VA~aE3sSJ1Abb1Q',
+}
+targetProduct = 'Azure Sentinel'
+misp_event_filters = {
+    'org': '',
+    'category': ''
+}
+action = 'alert'
+passiveOnly = False
+days_to_expire = 5
+misp_key = '465kvHQC1b4TGH2a6GUeA6SyxyjTdliSJIKl6iNt'
+misp_domain = 'https://localhost'
+misp_verifycert = False
+
